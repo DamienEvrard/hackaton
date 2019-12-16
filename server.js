a6ba3bbb9e397ca567cc84fad7a829bfa33afd63
DamienEvrard <damien.31861@laposte.net>
2019-12-16 17:14:55 +0100

    mise en forme du formulaire

--- server.js
+++ server.js
@@ -31,39 +31,8 @@ app.get('/personnels', function(req, res) {
 	});
 });
 
-app.post('/qualifie', function(req, res) {
-
-	comp1 = req.body.comp1;
-	comp2 = req.body.comp2;
-	comp3 = req.body.comp3;
-	val1=req.body.val1;
-	val2=req.body.val2;
-	val3=req.body.val3;
-
-	requete=""
-
-	if(comp1!=null){
-		requete+="and C.competence ="+comp1
-	}
-	if(comp2!=null){
-		requete+=", and C.competence ="+comp2
-	}
-	if(comp3!=null){
-		requete+=", and C.competence ="+comp3
-	}
-	if(val1!=null){
-		requete+=", and CP.pourcentAcquis ="+val1
-	}
-	if(val2!=null){
-		requete+=", and CP.pourcentAcquis ="+val2
-	}
-	if(val3!=null){
-		requete+=", and CP.pourcentAcquis ="+val3
-	}
-
-
-
-	let sql = 'SELECT * FROM Personnels P, Competences C, CompetencesPersonnels CP where P.id=CP.fk_id_personnel and CP.fk_id_competence=C.id '+requete;
+app.get('/allPoste', function(req, res) {
+	let sql = 'SELECT * FROM Postes';
 	
 	db.all(sql, [], (err, rows) => {
 		if (err) {
@@ -73,6 +42,17 @@ app.post('/qualifie', function(req, res) {
 	});
 });
 
+
+app.get('/personnelsOK', function(req, res) {
+	let sql = 'SELECT * FROM Personnels';
+	
+	db.all(sql, [], (err, rows) => {
+		if (err) {
+			throw err;
+		}
+		res.send(rows);
+	});
+});
 
 
 var port = 8080;
