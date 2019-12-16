089dfd271cfc490d742e2f06b32200c8c931cc6a
DamienEvrard <damien.31861@laposte.net>
2019-12-16 14:41:43 +0100

    fonction qualifie

--- server.js
+++ server.js
@@ -32,9 +32,35 @@ app.get('/personnels', function(req, res) {
 });
 
 app.post('/qualifie', function(req, res) {
-	req.body.comp1
+	comp1 = req.body.comp1;
+	comp2 = req.body.comp2;
+	comp3 = req.body.comp3;
+	val1=req.body.val1;
+	val2=req.body.val2;
+	val3=req.body.val3;
 
-	let sql = 'SELECT * FROM Personnels';
+	requete=""
+
+	if(comp1!=null){
+		requete+="C.competence ="+comp1
+	}
+	if(comp2!=null){
+		requete+=", and C.competence ="+comp2
+	}
+	if(comp3!=null){
+		requete+=", and C.competence ="+comp3
+	}
+	if(val1!=null){
+		requete+=", and CP.pourcentAcquis ="+val1
+	}
+	if(val2!=null){
+		requete+=", and CP.pourcentAcquis ="+val2
+	}
+	if(val3!=null){
+		requete+=", and CP.pourcentAcquis ="+val3
+	}
+
+	let sql = 'SELECT * FROM Personnels P, Competences C, CompetencesPersonnels CP where P.id=CP.fk_id_personnel and CP.fk_id_competence=C.id and '+requete;
 	db.all(sql, [], (err, rows) => {
 		if (err) {
 			throw err;
