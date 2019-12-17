7cbd870abed0022aab14e2b10299d8cf0e151ba6
Reguig <eruptor231998@gmail.com>
2019-12-17 14:25:20 +0100

    Tentative de remplacement du HEAD

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/', express.static(__dirname + '/public/'));
 app.use('/packages', express.static(__dirname + '/node_modules/'));
 
 app.get('/competences', function(req, res) {
-	let sql = 'SELECT libelle FROM Competences';
+	let sql = 'SELECT libelles FROM Competences';
 	db.all(sql, [], (err, rows) => {
 		if (err) {
 			throw err;
@@ -68,7 +68,7 @@ app.get('/allPoste', function(req, res) {
 app.get('/personnelsOK', function(req, res) {
 	let sql1 = 'SELECT * FROM (Personnels inner join CompetencesPersonnels on Personnels.id=CompetencesPersonnels.fk_id_personnel) inner join Competences on Competences.id=CompetencesPersonnels.fk_id_competence Where Personnels.prenoms="Bernard"  ';
 	let sql2='SELECT * FROM (Postes inner join FichePoste on Postes.id=FichePoste.fk_id_poste) inner join Competences on Competences.id=FichePoste.fk_id_competence ';
-	
+
 	id=req.query.id;
 	let sql ='select * from Personnels where (select C.libelles from Personnels Pers, CompetencesPersonnels CP, Competences C where Pers.id=CP.fk_id_personnel and CP.fk_id_competence=C.id) in (select C.libelles from Postes P, FichePoste FP, Competences C where P.id=FP.fk_id_poste and fk_id_competence=C.id and P.libelle='+id+')';
 
@@ -80,11 +80,11 @@ app.get('/personnelsOK', function(req, res) {
 	db.all(test, [], (err, rows) => {
 		if (err) {
 			throw err;
-		} 
+		}
 		results.push(rows);
 	});
 
-	
+
 
 	db.all(test2, [], (err, rows) => {
 		if (err) {
